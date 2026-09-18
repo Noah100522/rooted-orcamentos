9c83cf005d80c6227a05040865043076183043db
Noah100522 <danieleestefani0@gmail.com>
2026-09-17 21:52:55 -0300

    feat: add PHP database configuration example

---
+++ api/config.example.php
@@ -0,0 +1,9 @@
+<?php
+// Copie para config.php e ajuste conforme seu XAMPP/WAMP.
+return [
+  'host' => '127.0.0.1',
+  'port' => '3306',
+  'database' => 'rooted_orcamentos',
+  'username' => 'root',
+  'password' => '',
+];
